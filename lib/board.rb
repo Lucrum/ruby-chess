a422805ab6dd4a70896ce23bf598e61262168427
Elton <28992495+Lucrum@users.noreply.github.com>
2023-08-30 13:07:00 -0400

    Board setup, Unicode pieces

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,27 @@
 
 # class for the chess board
 class Board
-  def initialize
-    @state = Array.new(8) { Array.new(8) }
+  def initialize(state = nil)
+    @state = state
+    @encodings = { 'wk' => "\u2654", 'wq' => "\u2655", 'wr' => "\u2656",
+                   'wb' => "\u2657", 'wn' => "\u2658", 'wp' => "\u2659",
+                   'bk' => "\u265A", 'bq' => "\u265B", 'br' => "\u265C",
+                   'bb' => "\u265D", 'bn' => "\u265E", 'bp' => "\u265F" }
+  end
+
+  def setup
+    @state = [%w[wr wn wb wq wk wb wn wr],
+              %w[wp wp wp wp wp wp wp wp],
+              [nil, nil, nil, nil, nil, nil, nil, nil],
+              [nil, nil, nil, nil, nil, nil, nil, nil],
+              [nil, nil, nil, nil, nil, nil, nil, nil],
+              [nil, nil, nil, nil, nil, nil, nil, nil],
+              %w[bp bp bp bp bp bp bp bp],
+              %w[br bn bb bq bk bb bn br]]
+    @encodings
+  end
+
+  def encode_piece(piece)
+    @encodings[piece]
   end
 end
